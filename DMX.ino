b1a1d72ee5cd4469c4910260477b640a7248cb18
Brian Stubbs <stentroad@gmail.com>
2015-08-04 12:20:36 -0300

    Basic test of including DMXSerial library.

--- DMX.ino
+++ DMX.ino
@@ -1,51 +1,21 @@
-/*
-  Software serial multple serial test
-
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Nrot all pins on the Leonardo support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
- */
 #include <SoftwareSerial.h>
+#include <DMXSerial.h>
 
 #define rxPin 10
 #define txPin 11
 SoftwareSerial soft(rxPin, txPin);
 
-void setup()
-{
+void setup() {
     pinMode(13, OUTPUT);
-    Serial.begin(4800);
+    DMXSerial.init(DMXReceiver);
+    DMXSerial.write(1, 80);
     soft.begin(4800);
-    Serial.println("Hardware");
-    soft.println("Software");
+    soft.println("DMX setup");
 }
 
-void loop() // run over and over
-{
+void loop() {
     if (soft.available()) {
-        digitalWrite(13, HIGH);   // set the LED on
-        Serial.write(soft.read());
+        digitalWrite(13, HIGH);
+        soft.read();
     }
-    if (Serial.available())
-        soft.write(Serial.read());
 }
--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 # Arduino Make file. Refer to https://github.com/sudar/Arduino-Makefile
 
-ARDUINO_LIBS = SoftwareSerial
+USER_LIB_PATH = ./libraries
+ARDUINO_LIBS = SoftwareSerial DMXSerial
 ARDUINO_DIR = /home/brian/build/arduino_ide/arduino-1.6.4
-AVRDUDE = /usr/bin/avrdude 
+AVRDUDE = /usr/bin/avrdude
 BOARD_TAG    = uno
 include /home/brian/build/Arduino-Makefile/Arduino.mk
