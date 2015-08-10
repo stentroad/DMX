8270b7797c747ae44157bdc56a71c39388f9bb6f
Brian Stubbs <stentroad@gmail.com>
2015-08-10 10:06:19 -0300

    First test of receiving sACN (DMX over UDP) packets.

--- DMX.ino
+++ DMX.ino
@@ -1,27 +1,57 @@
-#include <SoftwareSerial.h>
-#include <DMXSerial.h>
+#include <Ethernet.h>
 
-#define rxPin 10
-#define txPin 11
-SoftwareSerial soft(rxPin, txPin);
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(239, 255, 0, 1);
+unsigned int localPort = 5568;
+
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+char ReplyBuffer[] = "acknowledged";       // a string to send back
+
+EthernetUDP Udp;
 
 void setup() {
-    pinMode(13, OUTPUT);
-    DMXSerial.init(DMXReceiver);
-    DMXSerial.write(1, 80);
-    soft.begin(4800);
-    soft.println("DMX setup");
+  Ethernet.begin(mac,ip);
+  Udp.begin(localPort);
+  Serial.begin(9600);
+  Serial.println("Setup complete...");
 }
 
 void loop() {
-    /* if (soft.available()) { */
-    /*     soft.read(); */
-    /* } */
-    unsigned long lastPacket = DMXSerial.noDataSince();
-    if (lastPacket < 5000) {
-        digitalWrite(13, HIGH);
-    } else {
-        digitalWrite(13, LOW);
-        soft.println("DMX disconnected...");
+  int packetSize = Udp.parsePacket();
+  if(packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i =0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
     }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
+    Serial.println("Contents:");
+    char buf[20];
+    for(int i = 0; i < packetSize; i++) {
+        sprintf(buf,"%02x ",(unsigned char)packetBuffer[i]);
+        if (i % 16 == 0) {
+            Serial.println();
+        }
+        Serial.print(buf);
+    }
+    Serial.println(packetBuffer);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+  delay(10);
 }
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # Arduino Make file. Refer to https://github.com/sudar/Arduino-Makefile
 
 USER_LIB_PATH = ./libraries
-ARDUINO_LIBS = SoftwareSerial DMXSerial
+ARDUINO_LIBS = SPI Ethernet # DMXSerial
 ARDUINO_DIR = /home/brian/build/arduino_ide/arduino-1.6.4
 AVRDUDE = /usr/bin/avrdude
 BOARD_TAG    = uno
