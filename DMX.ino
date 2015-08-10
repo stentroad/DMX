dc5a70f4ddea6a823f7ebd5a6e06cd6796e00508
Brian Stubbs <stentroad@gmail.com>
2015-08-10 15:41:41 -0300

    Basic: Parsing of first 4 DMX channels working.

--- DMX.ino
+++ DMX.ino
@@ -1,4 +1,8 @@
+#include <string.h>
 #include <Ethernet.h>
+#define UDP_TX_PACKET_MAX_SIZE 800
+#include <utility/util.h>
+#include <stdint.h>
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -7,16 +11,85 @@ IPAddress ip(239, 255, 0, 1);
 unsigned int localPort = 5568;
 
 // buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 char ReplyBuffer[] = "acknowledged";       // a string to send back
 
 EthernetUDP Udp;
 
+#define CHAN(n) packet.rlp.e131_packet.dmp_packet.property_values[n]
+#define CHAN_COUNT() packet.rlp.e131_packet.dmp_packet.property_value_count
+#define MAX_CHANNELS 512
+typedef struct DMP_PACKET {
+    uint16_t flags_and_length;
+    uint8_t vector;
+    uint8_t address_type_and_data_type;
+    uint16_t first_property_address;
+    uint16_t address_increment;
+    uint16_t property_value_count;
+    uint8_t property_values[MAX_CHANNELS+1];
+} DMP_PACKET_T;
+
+typedef struct E131_PACKET {
+    uint16_t flags_and_length;
+    uint32_t vector;
+    uint8_t source_name[64];
+    uint8_t priority;
+    uint16_t reserved;
+    uint8_t sequence_number;
+    uint8_t options;
+    uint16_t universe;
+    DMP_PACKET_T dmp_packet;
+} E131_PACKET_T;
+
+typedef struct RLP_PACKET {
+    uint16_t preamble_size;
+    uint16_t postamble_size;
+    uint8_t packet_identifier[12];
+    uint16_t flags_and_length;
+    uint32_t vector;
+    uint8_t component_identifier[16];
+    E131_PACKET_T e131_packet;
+} RLP_PACKET_T;
+
+typedef union DMX_PACKET {
+    char udp_buffer[UDP_TX_PACKET_MAX_SIZE];
+    RLP_PACKET_T rlp;
+} DMX_PACKET_T;
+
+DMX_PACKET_T packet;
+    
 void setup() {
   Ethernet.begin(mac,ip);
   Udp.begin(localPort);
   Serial.begin(9600);
   Serial.println("Setup complete...");
+}
+
+void print8d(const char* field_name,uint8_t field) {
+    char buf[20];
+    Serial.print(field_name);
+    sprintf(buf,"%u",field);
+    Serial.println(buf);
+}
+
+void print16d(const char* field_name,uint16_t field) {
+    char buf[20];
+    Serial.print(field_name);
+    sprintf(buf,"%u",ntohs(field));
+    Serial.println(buf);
+}
+
+void print16(const char* field_name,uint16_t field) {
+    char buf[20];
+    Serial.print(field_name);
+    sprintf(buf,"%04x",ntohs(field));
+    Serial.println(buf);
+}
+
+void print32(const char* field_name,uint32_t field) {
+    char buf[20];
+    Serial.print(field_name);
+    sprintf(buf,"%08lx",ntohl(field));
+    Serial.println(buf);
 }
 
 void loop() {
@@ -34,24 +107,40 @@ void loop() {
     }
     Serial.print(", port ");
     Serial.println(Udp.remotePort());
-
-    // read the packet into packetBufffer
-    Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Contents:");
-    char buf[20];
-    for(int i = 0; i < packetSize; i++) {
-        sprintf(buf,"%02x ",(unsigned char)packetBuffer[i]);
-        if (i % 16 == 0) {
-            Serial.println();
-        }
-        Serial.print(buf);
-    }
-    Serial.println(packetBuffer);
-
+    memset(packet.udp_buffer,0,sizeof(DMX_PACKET));
+    // read the packet into packet_buffer
+    Udp.read(packet.udp_buffer,UDP_TX_PACKET_MAX_SIZE);
+    /* Serial.println("Contents:"); */
+    /* char buf[20]; */
+    /* for(int i = 0; i < packetSize; i++) { */
+    /*     sprintf(buf,"%02x ",(unsigned char)packet.udp_buffer[i]); */
+    /*     if (i % 16 == 0) { */
+    /*         Serial.println(); */
+    /*     } */
+    /*     Serial.print(buf); */
+    /* } */
+    Serial.println();
+    /* Serial.print("Distance: "); */
+    /* unsigned int dist = (unsigned char*)&packet.rlp.e131_packet - (unsigned char*)&packet.rlp; */
+    /* Serial.println(dist, DEC); */
+    Serial.println("RLP-------------------------");
+    print16("Preamble size: ",packet.rlp.preamble_size);
+    print16("Postamble size: ",packet.rlp.postamble_size);
+    print16("Flags and length: ",packet.rlp.flags_and_length);
+    print32("Vector: ",packet.rlp.vector);
+    Serial.println("E131------------------------");
+    print16("Flags and length: ",packet.rlp.e131_packet.flags_and_length);
+    print32("Vector: ",packet.rlp.e131_packet.vector);
+    Serial.println("================================");
+    print16d("DMX channel count: ",CHAN_COUNT());
+    print8d("DMX channel 1: ",CHAN(1));
+    print8d("DMX channel 2: ",CHAN(2));
+    print8d("DMX channel 3: ",CHAN(3));
+    print8d("DMX channel 4: ",CHAN(4));
     // send a reply, to the IP address and port that sent us the packet we received
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(ReplyBuffer);
-    Udp.endPacket();
+    // Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    // Udp.write(ReplyBuffer);
+    // Udp.endPacket();
   }
   delay(10);
 }
