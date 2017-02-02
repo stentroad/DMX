bbb511efa5258e2399a57d4414ff93bb161837ff
Brian Stubbs <stentroad@gmail.com>
2017-02-02 11:57:59 -0200

    Allow debugging via defining SERIAL_DEBUG. Disable DMX. Fix for off by one error in DMX loop index.

--- DMX.ino
+++ DMX.ino
@@ -3,15 +3,18 @@
 #define UDP_TX_PACKET_MAX_SIZE 800
 #include <utility/util.h>
 #include <stdint.h>
-#include <DMXSerial.h>
+//#include <DMXSerial.h>
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(239, 255, 0, 1);
+//IPAddress ip(239, 255, 0, 1);
+IPAddress ip(192, 168, 1, 100);
 unsigned int localPort = 5568;
 
 EthernetUDP Udp;
+
+//#define SERIAL_DEBUG 1
 
 #define CHAN(n) packet.rlp.e131_packet.dmp_packet.property_values[n]
 #define CHAN_COUNT() packet.rlp.e131_packet.dmp_packet.property_value_count
@@ -23,7 +26,7 @@ typedef struct DMP_PACKET {
   uint16_t first_property_address;
   uint16_t address_increment;
   uint16_t property_value_count;
-  uint8_t property_values[MAX_CHANNELS+1];
+  uint8_t property_values[1+MAX_CHANNELS]; // CHAN(0) = DMX512-A START Code, CHAN(1-512) channel data
 } DMP_PACKET_T;
 
 typedef struct E131_PACKET {
@@ -60,92 +63,99 @@ const int BLUE_LED_PIN = 6;
 
 void setup() {
   pinMode(GREEN_LED_PIN, OUTPUT);
-  digitalWrite(GREEN_LED_PIN, LOW);
+  analogWrite(GREEN_LED_PIN, 0);
   Ethernet.begin(mac,ip);
   Udp.begin(localPort);
-  //Serial.begin(9600);
-  //Serial.println("Setup complete...");
-  DMXSerial.init(DMXController);
-  DMXSerial.write(2,255); // Red
+#ifdef SERIAL_DEBUG
+  Serial.begin(9600);
+  Serial.println("Setup complete...");
+#endif
+  //DMXSerial.init(DMXController);
+  //DMXSerial.write(2,255); // Red
   
 }
 
-// void print8d(const char* field_name,uint8_t field) {
-//     char buf[20];
-//     Serial.print(field_name);
-//     sprintf(buf,"%u",field);
-//     Serial.println(buf);
-// }
-//
-// void print16d(const char* field_name,uint16_t field) {
-//     char buf[20];
-//     Serial.print(field_name);
-//     sprintf(buf,"%u",ntohs(field));
-//     Serial.println(buf);
-// }
-//
-// void print16(const char* field_name,uint16_t field) {
-//     char buf[20];
-//     Serial.print(field_name);
-//     sprintf(buf,"%04x",ntohs(field));
-//     Serial.println(buf);
-// }
+#ifdef SERIAL_DEBUG
+void print8d(const char* field_name,uint8_t field) {
+    char buf[20];
+    Serial.print(field_name);
+    sprintf(buf,"%u",field);
+    Serial.println(buf);
+}
 
-// void print32(const char* field_name,uint32_t field) {
-//                                                      char buf[20];
-//                                                      Serial.print(field_name);
-//                                                      sprintf(buf,"%08lx",ntohl(field));
-//                                                      Serial.println(buf);
-//                                                      }
+void print16d(const char* field_name,uint16_t field) {
+    char buf[20];
+    Serial.print(field_name);
+    sprintf(buf,"%u",ntohs(field));
+    Serial.println(buf);
+}
+
+void print16(const char* field_name,uint16_t field) {
+    char buf[20];
+    Serial.print(field_name);
+    sprintf(buf,"%04x",ntohs(field));
+    Serial.println(buf);
+}
+
+void print32(const char* field_name,uint32_t field) {
+                                                     char buf[20];
+                                                     Serial.print(field_name);
+                                                     sprintf(buf,"%08lx",ntohl(field));
+                                                     Serial.println(buf);
+                                                     }
+#endif
 
 void loop() {
   int packetSize = Udp.parsePacket();
   if(packetSize) {
     //digitalWrite(GREEN_LED_PIN, HIGH);
-
-    /* Serial.print("Received packet of size "); */
-    /* Serial.println(packetSize); */
-    /* Serial.print("From "); */
-    /* IPAddress remote = Udp.remoteIP(); */
-    /* for (int i =0; i < 4; i++) { */
-    /*   Serial.print(remote[i], DEC); */
-    /*   if (i < 3) { */
-    /*     Serial.print("."); */
-    /*   } */
-    /* } */
-    /* Serial.print(", port "); */
-    /* Serial.println(Udp.remotePort()); */
-    /* memset(packet.udp_buffer,0,sizeof(DMX_PACKET)); */
+#ifdef SERIAL_DEBUG
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i =0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+    memset(packet.udp_buffer,0,sizeof(DMX_PACKET));
+#endif
     // read the packet into packet_buffer
     Udp.read(packet.udp_buffer,UDP_TX_PACKET_MAX_SIZE);
-    /* Serial.println("Contents:"); */
-    /* char buf[20]; */
-    /* for(int i = 0; i < packetSize; i++) { */
-    /*     sprintf(buf,"%02x ",(unsigned char)packet.udp_buffer[i]); */
-    /*     if (i % 16 == 0) { */
-    /*         Serial.println(); */
-    /*     } */
-    /*     Serial.print(buf); */
-    /* } */
-    /* Serial.println(); */
-    /* Serial.println("RLP-------------------------"); */
-    /* print16("Preamble size: ",packet.rlp.preamble_size); */
-    /* print16("Postamble size: ",packet.rlp.postamble_size); */
-    /* print16("Flags and length: ",packet.rlp.flags_and_length); */
-    /* print32("Vector: ",packet.rlp.vector); */
-    /* Serial.println("E131------------------------"); */
-    /* print16("Flags and length: ",packet.rlp.e131_packet.flags_and_length); */
-    /* print32("Vector: ",packet.rlp.e131_packet.vector); */
-    /* Serial.println("================================"); */
-    /* print16d("DMX channel count: ",CHAN_COUNT()); */
-    /* print8d("DMX channel 1: ",CHAN(1)); */
-    /* print8d("DMX channel 2: ",CHAN(2)); */
-    /* print8d("DMX channel 3: ",CHAN(3)); */
-    /* print8d("DMX channel 4: ",CHAN(4)); */
-    for(int i=0;i<CHAN_COUNT();i++) {
-      DMXSerial.write(i,CHAN(i));
+#ifdef SERIAL_DEBUG
+    Serial.println("Contents:");
+    char buf[20];
+    for(int i = 0; i < packetSize; i++) {
+        sprintf(buf,"%02x ",(unsigned char)packet.udp_buffer[i]);
+        if (i % 16 == 0) {
+            Serial.println();
+        }
+        Serial.print(buf);
     }
-    analogWrite(GREEN_LED_PIN, CHAN(0));
+    Serial.println();
+    Serial.println("RLP-------------------------");
+    print16("Preamble size: ",packet.rlp.preamble_size);
+    print16("Postamble size: ",packet.rlp.postamble_size);
+    print16("Flags and length: ",packet.rlp.flags_and_length);
+    print32("Vector: ",packet.rlp.vector);
+    Serial.println("E131------------------------");
+    print16("Flags and length: ",packet.rlp.e131_packet.flags_and_length);
+    print32("Vector: ",packet.rlp.e131_packet.vector);
+    Serial.println("================================");
+    print16d("DMX channel count: ",CHAN_COUNT());
+    print8d("DMX channel 1: ",CHAN(1));
+    print8d("DMX channel 2: ",CHAN(2));
+    print8d("DMX channel 3: ",CHAN(3));
+    print8d("DMX channel 4: ",CHAN(4));
+#endif
+    // for(int i=1;i<=CHAN_COUNT();i++) {
+    //   DMXSerial.write(i,CHAN(i));
+    // }
+    analogWrite(GREEN_LED_PIN, CHAN(1));
     //digitalWrite(GREEN_LED_PIN, LOW);
   }
   delayMicroseconds(2000);
